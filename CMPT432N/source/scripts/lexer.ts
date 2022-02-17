501a559447fa0e9ce013ec4b2a60b2d6625af75e
Devin <whitedevin37@gmail.com>
2022-02-16 19:12:44 -0500

    Regular Expression Added

--- CMPT432N/source/scripts/lexer.ts
+++ CMPT432N/source/scripts/lexer.ts
@@ -3,15 +3,43 @@
 module TSC
 	{
 	export class Lexer {
-		public static lex() {
-		    {
+
+        public static lex(){
+		{
 		        // Grab the "raw" source code.
 		        var sourceCode = (<HTMLInputElement>document.getElementById("taSourceCode")).value;
 		        // Trim the leading and trailing spaces.
-		        sourceCode = TSC.Utils.trim(sourceCode);
-		        // TODO: remove all spaces in the middle; remove line breaks too.
-		        return sourceCode;
-		    }
+				sourceCode = TSC.Utils.trim(sourceCode);
+				
+				// Declare Regular Expressions based on our grammar.
+
+				const L_BRACE = new RegExp('{$');
+				const R_BRACE = new RegExp('}$');
+				const EOP = new RegExp('$$');
+				const L_PAREN = new RegExp('($');
+				const R_PAREN = new RegExp(')$');
+				const PRINT = new RegExp('print$');
+				const WHILE = new RegExp('while$');
+				const IF = new RegExp('if$');
+				const INT_TYPE = new RegExp('int$');
+				const BOOL_TYPE = new RegExp('boolean$');
+				const STRING_TYPE = new RegExp('string$');
+				const DOULBE_QUOTE = new RegExp('"$');
+				const VARIABLE = new RegExp('[a-z]$');
+				const ASSIGN = new RegExp('=$');
+                const SPACE = new RegExp(' $');
+				const NEWLINE = new RegExp('\n$');
+				const DIGIT = new RegExp('[0-9]$');
+				const INTOP = new RegExp('+$');
+				const BOOL_TRUE = new RegExp('true$');
+				const BOOL_FALSE = new RegExp('false$');
+				const BOOL_EQUAL = new RegExp('==$');
+				const BOOL_NOTEQUAL = new RegExp('!=$');
+				const BEGINCOMMENT = new RegExp('/*$');
+				const ENDCOMMENT = new RegExp('*/$');
+
+				return sourceCode;
+			}
 		}
 	}
-	}
+}
