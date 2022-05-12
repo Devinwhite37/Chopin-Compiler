45cfbb56c4a7060c0dbc9a9e710f08d7434f3317
Devin <whitedevin37@gmail.com>
2022-05-11 20:00:46 -0400

    addition of numbers is working

--- CMPT432N/source/scripts/codeGen.js
+++ CMPT432N/source/scripts/codeGen.js
@@ -66,22 +66,16 @@ var TSC;
                 }
             }
             else if (node.name == 'PrintStatement') {
+                console.log(node.children[0].value);
                 this.codeGenLog.push("Generating op code for PrintStatement:");
                 //tests if the value in the print is a digit
-                if (DIGIT.test(node.children[0].name[0])) {
-                    this.setHex("A0");
-                    this.setHex("0" + node.children[0].name[0]);
-                    this.setHex("A2");
-                    this.setHex("01");
-                }
                 // tests if the value in print is a variable
-                else if (node.children[0].value == 'variable') {
+                if (node.children[0].value == 'variable') {
                     this.setHex("AC");
                     var variable = node.children[0].name[0];
                     var scope = node.children[0].scope;
                     var staticVal = this.findVariable(variable, scope);
                     this.setHex(staticVal);
-                    console.log(staticVal);
                     this.setHex("00");
                     for (var j = 0; j < this.staticTable.length; j++) {
                         if (this.staticTable[j][0].value == staticVal) {
@@ -116,6 +110,21 @@ var TSC;
                     }
                     this.setHex("A2");
                     this.setHex("02");
+                }
+                else if (node.children[0].value == 'digit' && node.children[1] === undefined) {
+                    this.setHex("A0");
+                    this.setHex("0" + node.children[0].name[0]);
+                    this.setHex("A2");
+                    this.setHex("01");
+                }
+                if (node.children[1] === undefined) { }
+                else if (node.children[1].name == "ADDITION_OP") {
+                    this.additionOp(node.children);
+                    this.setHex("AC");
+                    this.setHex("00");
+                    this.setHex("00");
+                    this.setHex("A2");
+                    this.setHex("01");
                 }
                 else if (node.children[1].name[0] == 'BOOL_EQUAL') {
                 }
@@ -170,6 +179,31 @@ var TSC;
             }
             //this.traverse(node.parent.children[1]);
         };
+        CodeGen.prototype.additionOp = function (node) {
+            console.log("node:");
+            console.log(node);
+            if (node[1] === undefined) { }
+            else if (node[1].name == 'ADDITION_OP') {
+                this.additionOp(node[1].children);
+            }
+            if (node[0].value == 'digit') {
+                this.setHex("A9");
+                this.setHex("0" + node[0].name[0]);
+                this.setHex("8D");
+                this.setHex("00");
+                this.setHex("00");
+            }
+            if (node[0].value == 'digit') {
+                this.setHex("A9");
+                this.setHex("0" + node[0].name[0]);
+            }
+            this.setHex("6D");
+            this.setHex("00");
+            this.setHex("00");
+            this.setHex("8D");
+            this.setHex("00");
+            this.setHex("00");
+        };
         CodeGen.prototype.staticArea = function () {
             this.staticAreaLocation = this.hexLocation + 1;
             var staticVarsLength = this.staticTable.length;
@@ -184,7 +218,6 @@ var TSC;
                 this.staticAreaLocation++;
                 //}
             }
-            console.log(this.staticTable);
         };
         CodeGen.prototype.backPatch = function () {
             for (var i = 0; i < this.createdCode.length; i++) {
@@ -192,12 +225,9 @@ var TSC;
                 if (this.createdCode[i] === undefined) { }
                 else if (this.createdCode[i].charAt(0) == "T") {
                     var patchVal = this.createdCode[i];
-                    console.log(patchVal);
                     for (var j = 0; j < this.staticTable.length; j++) {
                         if (this.staticTable[j][0].value == patchVal) {
                             var memAddr = this.staticTable[j][0].byteNum;
-                            console.log(memAddr);
-                            console.log(this.createdCode[i]);
                             this.createdCode[i] = memAddr;
                             this.codeGenLog.push("BackPatching " + patchVal + " with " + memAddr);
                         }
@@ -206,9 +236,10 @@ var TSC;
             }
         };
         CodeGen.prototype.findVariable = function (variable, scope) {
+            console.log(this.staticTable);
             for (var i = 0; i < this.staticTable.length; i++) {
                 //var staticObject = itr.next();
-                if (this.staticTable[i][0].key == variable && this.staticTable[i][0].scope == scope) {
+                if (this.staticTable[i][0].key == variable && this.staticTable[i][0].scope <= scope) {
                     return this.staticTable[i][0].value;
                 }
             }
